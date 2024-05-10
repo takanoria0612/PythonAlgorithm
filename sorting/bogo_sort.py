c847cdde4fd07b0f734d5f7698346ea575880617
takanoria0612 <takanorihiraki06@gmail.com>
2024-05-11 00:53:33 +0900

    type hits added

--- sorting/bogo_sort.py
+++ sorting/bogo_sort.py
@@ -1,12 +1,13 @@
 from random import shuffle
+from typing import List
 
 # Function to check if a list is sorted
-def is_sorted(numbers):
+def is_sorted(numbers:List[int]) -> bool:
     # Check if each element is less than or equal to the next element
     return all(numbers[i] <= numbers[i + 1] for i in range(len(numbers) - 1))
 
 # Bogo Sort function
-def bogo_sort(numbers):
+def bogo_sort(numbers:List[int]) -> List[int]:
     # Shuffle the list until it is sorted
     while not is_sorted(numbers):
         shuffle(numbers)
