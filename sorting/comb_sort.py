a70998c6e8198d4763d5aba0089dba481cce8a97
takanoria0612 <takanorihiraki06@gmail.com>
2024-05-18 16:42:35 +0900

    combソート実装開始、設計書追加

---
+++ sorting/comb_sort.py
@@ -0,0 +1,5 @@
+from typing import List
+
+def comb_sort(numbers: List[int]) -> List[int]:
+    return numbers
+
---
+++ tests/test_comb_sort.py
@@ -0,0 +1,7 @@
+import pytest
+from sorting.comb_sort import comb_sort
+
+def test_empty_list():
+    assert comb_sort([]) == []
+
+
